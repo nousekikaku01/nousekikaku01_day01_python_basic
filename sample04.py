efd52b4a351b862e1b1f03e651e7e95efe86c4f1
nousekikaku01 <nousekikaku01@gmail.com>
2020-12-09 16:44:22 +0900

    再開

--- sample04.py
+++ sample04.py
@@ -1,5 +1,9 @@
 # ２つの整数の演算いくつか紹介
-print('3+4')
-print('3-4')
-print('3*4')
-print('3/4')
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(3 / 4 + 1)
+print(3 + 3 + 3+3)
+print(1+2)
+print(1+2)
