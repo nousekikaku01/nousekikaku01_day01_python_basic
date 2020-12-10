1aef5096f9dbb7603851943832c9f851422773e2
nousekikaku01 <nousekikaku01@gmail.com>
2020-12-10 09:44:36 +0900

    if-else #条件分岐

---
+++ sample10.py
@@ -0,0 +1,11 @@
+# ifにまつわるあるあるエラーのコーナー
+# コロン忘れ
+total_price = 888  # ここの値を変えてみよう
+
+# if total_price == 777
+#    print('Lucky!!!')
+
+# ex2
+# IndentationErrorは「字下げ」（インデント）
+if total_price == 777:
+print('Lucky!!!')
---
+++ sample11.py
@@ -0,0 +1,7 @@
+# 条件分岐その２：if-else
+score = 60
+
+if score >= 70:
+    print('Great')
+else:
+    print('Bad...')
